e7b9b029690cd8bebd21f386f40b28a37787d73e
Ellery-SH <77747645+Ellery-SH@users.noreply.github.com>
2021-02-06 21:29:33 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mobiledevelopment-ex2
+"Stop creating competitions that don't exist."
+-unknown
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes =[
+   `"Stop creating competitions that don't exist."
+   -unknown`,
+   `"Life's a garden, dig it."
+   -Joe Dirt, Joe dirt`,
+]
+let index=(Math.floor(Math.random() * 2))
+console.log(quotes[index])
